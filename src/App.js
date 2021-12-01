9b06518c38e97458830fe428b2371b02dea4df8e
Matheus Grativol <matheus_grati@hotmail.com>
2021-11-30 21:55:00 -0300

    Provavel esqueleto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    
+  }
+  
+  render(){
+    return (
+      <div>
+        <div>
+          <h1>Header</h1>
+        </div>
+
+        <div className="App">
+          <div>
+            <h3>Filtro</h3>
+            <label>
+              "Valor mínimo:"
+              <input type="number" value="50"/>
+            </label>
+            <label>
+              "Valor máximo:"
+              <input type="number" value="500"/>
+            </label>
+            <label>
+              "Buscar por nome:"
+              <input type="text" value="produto"/>
+            </label>
+          </div>
+
+          <div>
+            <h3>Pordutos</h3>
+          </div>
+
+          <div>
+            <h3>Carrinho</h3>
+          </div>
+
+        </div>
+        <div>Footer</div>
+      </div>
+      
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Produtos.js
@@ -0,0 +1,18 @@
+import React from "react";
+import App from "../App";
+class produtos extends React.Component {
+  
+    render(){
+      return (
+        <div>
+            <img src={}/>
+            <h4></h4>
+            <p></p>
+            <button>Adicionar ao carrinho</button>
+        </div>
+      );
+    }
+    
+  }
+  
+  export default produtos;
