cdd29709b4566f540d8c6f2c66322fac18960b2b
Matheus Grativol <matheus_grati@hotmail.com>
2021-12-01 20:37:56 -0300

    raculhos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
+import Produtos from './components/Produtos';
 
+const ContainerPrincipal = styled.div`
+  display: flex;
+  /* justify-content: space-between; */
+  /* flex-grow: 0 1 0; */
+`
+const ContainerFiltro = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0.6rem;
+  border: 1px red solid;
+  margin: 0 1rem;
+  height: 15rem;
+  background-color: whitesmoke;
+`
+const DivCards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr ;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  row-gap: 1rem;
+  column-gap: 2rem;
+  margin-left: 2rem;
+  /* background-color: whitesmoke; */
+`
+
+const DivCarrinho = styled.div`
+  margin-left: 3rem;
+`
 class App extends React.Component {
   state = {
-    
+    quantidade:"",
+    preco:"",
+    valorMinimo:"",
+    valorMaximo:"",
+    buscaNome:""
+  }
+
+  mudarMinimo = (event) =>{
+    this.setState({valorMinimo: event.target.value})
   }
   
+  mudarMaximo = (event) => {
+    this.setState({valorMaximo: event.target.value})
+  }
+
+  mudarBusca = (event) =>{
+    this.setState({buscaNome: event.target.value})
+  }
   render(){
     return (
       <div>
@@ -13,32 +57,81 @@ class App extends React.Component {
           <h1>Header</h1>
         </div>
 
-        <div className="App">
-          <div>
+        <ContainerPrincipal>
+          <ContainerFiltro>
             <h3>Filtro</h3>
             <label>
-              "Valor mínimo:"
-              <input type="number" value="50"/>
+              Valor mínimo:
+              <br/>
+              <input type="number" value={this.state.valorMinimo} onChange={this.mudarMinimo}/>
             </label>
             <label>
-              "Valor máximo:"
-              <input type="number" value="500"/>
+              Valor máximo:
+              <br/>
+              <input type="number" value={this.state.valorMaximo} onChange={this.mudarMaximo}/>
             </label>
             <label>
-              "Buscar por nome:"
-              <input type="text" value="produto"/>
+              Buscar por nome:
+              <br/>
+              <input type="text" placeholder="Pesquisa " value={this.state.buscaNome} onChange={this.mudarBusca}/>
             </label>
-          </div>
+          </ContainerFiltro>
 
-          <div>
-            <h3>Pordutos</h3>
-          </div>
+          <DivCards>
+            {/* <h3>Pordutos</h3> */}
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=1"}
+              nomeProduto={"Gato 1"}
+              // preco={this.state.preco}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=2"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=3"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=4"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=5"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=6"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=7"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=8"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+            <Produtos 
+              img={"https://loremflickr.com/180/200?lock=1"}
+              nomeProduto={"Gato 1"}
+              preco={"1.000"}
+            />
+          </DivCards>
 
-          <div>
-            <h3>Carrinho</h3>
-          </div>
+          <DivCarrinho>
+            <h3>Carrinho:</h3>
+          </DivCarrinho>
 
-        </div>
+        </ContainerPrincipal>
         <div>Footer</div>
       </div>
       
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,18 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import App from "../App";
-class produtos extends React.Component {
-  
+
+const ContainerCard = styled.div`
+  /* border: 1px black solid; */
+  box-shadow: 5px 3px 3px silver;
+  padding: 2px;
+  border-radius: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: whitesmoke;
+`
+const BotaoAddCarrinho = styled.button`
+  /* background-color: whitesmoke; */
+  margin-left: 20px;
+
+`
+
+class Produtos extends React.Component {
+
     render(){
       return (
-        <div>
-            <img src={}/>
-            <h4></h4>
-            <p></p>
-            <button>Adicionar ao carrinho</button>
-        </div>
+        <ContainerCard>
+            <img src={this.props.img}/>
+            <h4>{this.props.nomeProduto}</h4>
+            <p>R$ {this.props.preco}</p>
+            <BotaoAddCarrinho>Adicionar ao carrinho</BotaoAddCarrinho>
+        </ContainerCard>
       );
     }
     
   }
   
-  export default produtos;
+  export default Produtos;
